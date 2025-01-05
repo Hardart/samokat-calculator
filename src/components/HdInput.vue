2a0629872e8362d44d32eee3e5a62e08ab474bd5
Hardart <kaino@bk.ru>
2025-01-05 23:41:17 +0300

    feat: add custom input

---
+++ src/components/HdInput.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+const model = defineModel<number>({
+  required: true,
+  default: 0,
+  set(value) {
+    if (typeof value === 'string') value = 0
+    return value
+  },
+})
+defineProps<{
+  label?: string
+  id?: string
+}>()
+</script>
+
+<template>
+  <div>
+    <label :for="id" v-if="label">{{ label }}</label>
+    <div class="custom-input">
+      <input type="text" class="inputtext" :id v-model.number="model" />
+      <button
+        class="pi pi-plus inputnumber-button inputbutton-end"
+        @click="model++"
+      />
+      <button
+        class="pi pi-minus inputnumber-button inputbutton-start"
+        @click="model--"
+        :disabled="model == 0"
+      />
+    </div>
+  </div>
+</template>
