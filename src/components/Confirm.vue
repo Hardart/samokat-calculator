7ebb22e167f8d9d2c6738fd37bd649c36d71deec
Hardart <kaino@bk.ru>
2025-01-26 23:37:27 +0300

    feat: add new Confirm component

---
+++ src/components/Confirm.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import { Dialog, Button } from 'primevue'
+const isOpen = defineModel<boolean>({ required: true })
+defineEmits(['confirm', 'cancel'])
+</script>
+
+<template>
+  <Dialog
+    v-model:visible="isOpen"
+    position="bottom"
+    :closable="false"
+    content-class="confirm-dialog"
+  >
+    <Button
+      label="Отменить"
+      size="small"
+      icon="pi pi-times"
+      severity="danger"
+      @click="$emit('cancel')"
+    />
+    <Button
+      label="Подвердить"
+      size="small"
+      icon="pi pi-check"
+      @click="$emit('confirm')"
+    />
+  </Dialog>
+</template>
+
+<style>
+.confirm-dialog {
+  display: flex;
+  gap: 20px;
+}
+</style>
