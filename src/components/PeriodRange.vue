6c7f2c0b12aba9b3ae378a00b4d1ebc10bd46a17
Hardart <kaino@bk.ru>
2025-01-26 23:42:50 +0300

    feat: add new Period item and PeriodRange components and clenup code

---
+++ src/components/PeriodRange.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import { ButtonGroup, Button } from 'primevue'
+import { dayAndMonth } from '@/shared/date'
+import { isShowNextWeekRange, shiftWeekRange, weekRange } from '@/shared/utils'
+import { showMissedDates } from '@/shared/shiftData'
+const increaseRange = () => {
+  shiftWeekRange.value++
+  showMissedDates.value = false
+}
+const decreaseRange = () => {
+  shiftWeekRange.value--
+  showMissedDates.value = false
+}
+</script>
+
+<template>
+  <div class="period-range">
+    C {{ dayAndMonth(weekRange.startDate) }} по
+    {{ dayAndMonth(weekRange.endDate) }}
+    <div>
+      <ButtonGroup>
+        <Button
+          size="small"
+          icon="pi pi-angle-left"
+          @click="increaseRange"
+          variant="text"
+        />
+        <Button
+          size="small"
+          icon="pi pi-angle-right"
+          @click="decreaseRange"
+          :disabled="isShowNextWeekRange"
+          variant="text"
+        />
+      </ButtonGroup>
+    </div>
+  </div>
+</template>
--- src/components/Statistic.vue
+++ src/components/Statistic.vue
@@ -1,62 +1,59 @@
 <script lang="ts" setup>
-import { Dialog } from 'primevue'
-import { settings, shifts, shiftsTotal } from '@/shared/localData'
+import { Dialog, Button } from 'primevue'
+import PeriodItem from './PeriodItem.vue'
+import PeriodRange from './PeriodRange.vue'
+import { formatHours, formatOrders } from '@/shared/utils'
+import { settings } from '@/shared/localData'
 import {
-  formatHours,
-  formatOrders,
-  formattedDate,
-  isInPeriod,
-} from '@/shared/utils'
+  datesForPeriod,
+  shiftsForPeriod,
+  shiftsTotal,
+  showMissedDates,
+} from '@/shared/shiftData'
+import { formattedDate } from '@/shared/date'
 </script>
 
 <template>
   <Dialog
     v-model:visible="settings.isOpenStatistic"
     modal
-    header="Статистика"
-    :style="{ width: '25rem' }"
+    :style="{ width: '22rem' }"
   >
+    <template #header>
+      <div class="p-dialog-header p-dialog-header--custom">
+        <h3 class="p-dialog-title">Статистика</h3>
+      </div>
+    </template>
+    <PeriodRange />
     <div class="period-modal">
       <ul class="period-list">
-        <template v-for="shiftItem in shifts">
-          <li class="period" v-if="isInPeriod(shiftItem.date)">
-            <div class="period__weekday">
-              {{ formattedDate(true, new Date(shiftItem.date)) }}
-            </div>
-            <div>
-              {{ formatHours(shiftItem.hours) }},
-              {{ formatOrders(shiftItem.orders) }}
-            </div>
-
-            <div class="period__weather-data">
-              Надбавка за погоду -
-              <span
-                v-if="shiftItem.isWeather"
-                class="period__weather period__weather--on"
-              >
-                Да
-              </span>
-              <span v-else class="period__weather">Нет</span>
-            </div>
-            <div>
-              Получил чаевых
-              <span class="periiod__tips-value">{{ shiftItem.tips }}₽</span>
-            </div>
-            <div>
-              Заработал
-              <span class="periiod__profit-value">{{ shiftItem.profit }}₽</span>
-            </div>
-          </li>
-        </template>
+        <!-- <Button
+          v-if="!showMissedDates"
+          @click="showMissedDates = true"
+          label="Добавить данные"
+          severity="contrast"
+          size="small"
+        /> -->
+        <Button
+          v-if="showMissedDates"
+          v-for="dateItem in datesForPeriod"
+          :label="formattedDate(true, dateItem)"
+          severity="contrast"
+          variant="outlined"
+          size="small"
+        />
+        <PeriodItem v-for="(shiftItem, id) in shiftsForPeriod" :shiftItem :id />
       </ul>
-      <div class="period-total" v-if="shiftsTotal.hours">
+      <div class="period-total" v-if="shiftsForPeriod.length">
         <h3 class="period-total__title">За неделю</h3>
         <div>Отработал - {{ formatHours(shiftsTotal.hours) }}</div>
         <div>Выполнил - {{ formatOrders(shiftsTotal.orders) }}</div>
         <div>Получил чаевых - {{ shiftsTotal.tips }}₽</div>
         <div>Заработал - {{ shiftsTotal.profit }}₽</div>
       </div>
-      <h3 v-else>За этот период пока нет сохраненных данных</h3>
+      <h3 v-else class="period-empty">
+        За этот период пока нет сохраненных данных
+      </h3>
     </div>
   </Dialog>
 </template>
