8aacc01352c29b0f4b03c6520a27297ef4c577d0
Hardart <kaino@bk.ru>
2025-02-01 01:28:37 +0300

    refactor: update hours price value

--- src/shared/hoursData.ts
+++ src/shared/hoursData.ts
@@ -6,8 +6,8 @@ export const hoursData = useLocalStorage('hours', {
   hours: 0,
   EXTRA_MONEY_PER_HOUR: 15,
   EXTRA_MONEY_PER_HOUR_FOR_LASTWEEK: 10,
-  SCOOTER_HOUR_PRICE: 130,
-  BICYCLE_HOUR_PRICE: 110,
+  SCOOTER_HOUR_PRICE: 125,
+  BICYCLE_HOUR_PRICE: 105,
 })
 
 export const singleHourPrice = computed(() => {
