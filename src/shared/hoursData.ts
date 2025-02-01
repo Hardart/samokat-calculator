116dc3561420e74878b894308aeddeaaa6e61c63
Hardart <kaino@bk.ru>
2025-02-01 13:59:08 +0300

    refactor: move constants settings from local storage

--- src/shared/hoursData.ts
+++ src/shared/hoursData.ts
@@ -1,22 +1,23 @@
 import { useLocalStorage } from '@vueuse/core'
 import { computed } from 'vue'
 import { settings } from './localData'
+import { companyData } from './companyData'
+const { company } = companyData()
+
+export const EXTRA_MONEY_PER_HOUR = 15
+const EXTRA_MONEY_PER_HOUR_FOR_LASTWEEK = 10
+const SCOOTER_HOUR_PRICE = 120
+const BICYCLE_HOUR_PRICE = 100
 
 export const hoursData = useLocalStorage('hours', {
   hours: 0,
-  EXTRA_MONEY_PER_HOUR: 15,
-  EXTRA_MONEY_PER_HOUR_FOR_LASTWEEK: 10,
-  SCOOTER_HOUR_PRICE: 125,
-  BICYCLE_HOUR_PRICE: 105,
 })
 
 export const singleHourPrice = computed(() => {
-  let price = settings.value.isBicycle
-    ? hoursData.value.BICYCLE_HOUR_PRICE
-    : hoursData.value.SCOOTER_HOUR_PRICE
-  if (settings.value.isExtraDay) price += hoursData.value.EXTRA_MONEY_PER_HOUR
-  if (settings.value.isLastWeekHours)
-    price += hoursData.value.EXTRA_MONEY_PER_HOUR_FOR_LASTWEEK
+  let price = settings.value.isBicycle ? BICYCLE_HOUR_PRICE : SCOOTER_HOUR_PRICE
+  if (settings.value.isExtraDay) price += EXTRA_MONEY_PER_HOUR
+  if (settings.value.isLastWeekHours) price += EXTRA_MONEY_PER_HOUR_FOR_LASTWEEK
+  if (company.value) price += company.value.hourBonus
   return price
 })
 
